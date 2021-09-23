c56772d25f4d4829f85971cde7dbc725438011c3
ojhdt <ojhdtmail@gmail.com>
2021-09-23 10:37:08 +0800

    update

--- app/src/main/java/com/example/bingdaily/MainActivity.kt
+++ app/src/main/java/com/example/bingdaily/MainActivity.kt
@@ -2,14 +2,39 @@ package com.example.bingdaily
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import com.example.bingdaily.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar.let {
+            it?.setDisplayHomeAsUpEnabled(true)
+            it?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home->{binding.drawer.openDrawer(GravityCompat.START)}
+            R.id.refresh -> {// TODO: 2021/9/23
+            }
+            R.id.about -> {// TODO: 2021/9/23
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/bingdaily/logic/Repository.kt
+++ app/src/main/java/com/example/bingdaily/logic/Repository.kt
@@ -1,5 +1,6 @@
 package com.example.bingdaily.logic
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import com.example.bingdaily.logic.modal.ImageResponse
 import com.example.bingdaily.logic.network.BingNetwork
@@ -7,6 +8,6 @@ import com.example.bingdaily.logic.network.BingNetwork
 object Repository {
     fun getImage() = liveData {
         val result = BingNetwork.getImage()
-        emit(Result.success(result))
+        emit(Result.success(result) as LiveData<ImageResponse>)
     }
 }
